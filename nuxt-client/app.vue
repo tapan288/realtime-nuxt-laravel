9b33efdd361d0d066377b5cfb6d5749c9890768e
Tapan Sharma <tapansharma69@gmail.com>
2024-10-11 18:17:42 +0545

    private channels and broadcating auth

--- nuxt-client/app.vue
+++ nuxt-client/app.vue
@@ -1,12 +1,26 @@
 <script setup>
+const sanctumFetch = useSanctumClient();
+
 onMounted(() => {
-  Echo.channel("posts").listen("ExampleEvent", (e) => {
+  //   Echo.channel("posts").listen("ExampleEvent", (e) => {
+  //     console.log(e);
+  //   });
+
+  Echo.private("privateChannel").listen("PrivateEventExample", (e) => {
     console.log(e);
   });
+
+  const sendRequest = () => {
+    sanctumFetch("/api/realtime", {
+      method: "POST",
+    });
+  };
 });
 </script>
 
 <template>
   <Navigation />
   <div>Realtime</div>
+
+  <button v-on:click="sendRequest">Send request</button>
 </template>
--- realtime-nuxt-laravel/routes/api.php
+++ realtime-nuxt-laravel/routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Events\PrivateEventExample;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/realtime', function () {
+    broadcast(new PrivateEventExample());
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();

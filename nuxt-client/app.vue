b6ecfa6cabd970e8fb313969de2346b2090def0f
Tapan Sharma <tapansharma69@gmail.com>
2024-10-11 15:43:29 +0545

    listening for events

--- nuxt-client/app.vue
+++ nuxt-client/app.vue
@@ -1,3 +1,11 @@
+<script setup>
+onMounted(() => {
+  Echo.channel("posts").listen("ExampleEvent", (e) => {
+    console.log(e);
+  });
+});
+</script>
+
 <template>
   <div>Realtime</div>
 </template>
